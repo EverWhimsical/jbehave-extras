0171d3ec951accb9c5e755a84b73af9a980b627f
Bharath Kumar <23705200+bharathc02@users.noreply.github.com>
2018-11-09 08:13:39 +0800

    Added branch condition

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,9 @@
 echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
 echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
-echo "About to run deploy"
-mvn deploy --settings scripts/.maven.xml -DskipTests=true -B -U -Prelease
+echo "TRAVIS_BRANCH $TRAVIS_BRANCH | is_release_candidate $is_release_candidate"
+if [[ "$TRAVIS_BRANCH" == "master" && $is_release_candidate != 1 ]]; then
+    echo "Skipping deploy since it is not a release and branch is master"
+else
+    echo "About to run deploy"
+    mvn deploy --settings scripts/.maven.xml -DskipTests=true -B -U -Prelease
+fi
